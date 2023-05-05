af1710c4a9eaf70d5d3e80d58b2f3f45f0a5a985
Reid-T-W <rediettadesse100@gmail.com>
2023-05-05 17:51:23 +0300

    fully working and time optimized queue using two stacks

--- HackerRank/queueUsingTwoStacks.py
+++ HackerRank/queueUsingTwoStacks.py
@@ -3,31 +3,66 @@
 stack1 = []
 stack2 = []
 def execute_command(command):
+    # Approach 1 (deque more expensive than enque)
+    # but better than approach 1
     if command[0] == '1':
         stack1.append(command[1])
     elif command[0] == '2':
-        # Turn by turn remove all elements from stack1
-        # and add them to stack2, so that the first element of 
-        # stack1 will be at the top of stack2
-        while stack1:
-            element = stack1.pop()
-            stack2.append(element)
-        # Remove the top element in stack2, this means that
-        # the first element is removed
-        element = stack2.pop()
-        # Return the elements back to stack1
-        while stack2:
-            element = stack2.pop()
-            stack1.append(element)
+        # If stack 2 is not empty pop the element from stack2
+        if stack2:
+            stack2.pop()
+        # If stack 2 is empty and stack 1 is not move all elements
+        # in stack 1 to stack 2 and pop the last element
+        else:
+            if (stack1):
+                while stack1:
+                    element = stack1.pop()
+                    stack2.append(element)
+                element = stack2.pop()
+            else:
+                print("Queue is empty")
     elif command[0] == '3':
-        while stack1:
-            element = stack1.pop()
-            stack2.append(element)
-        print(stack2[-1])
-        while stack2:
-            element = stack2.pop()
-            stack1.append(element)   
+        # If stack 2 is not empty pop the element from stack2
+        if stack2:
+            print(stack2[-1])
+        # If stack 2 is empty and stack 1 is not move all elements
+        # in stack 1 to stack 2 and pop the last element
+        else:
+            if (stack1):
+                while stack1:
+                    element = stack1.pop()
+                    stack2.append(element)
+                print(stack2[-1])
+            else:
+                print("Queue is empty")
 
+    # Approach 2 (Very Expensive deque operation)
+    # if command[0] == '1':
+    #     stack1.append(command[1])
+    # elif command[0] == '2':
+    #     # Turn by turn remove all elements from stack1
+    #     # and add them to stack2, so that the first element of 
+    #     # stack1 will be at the top of stack2
+    #     while stack1:
+    #         element = stack1.pop()
+    #         stack2.append(element)
+    #     # Remove the top element in stack2, this means that
+    #     # the first element is removed
+    #     element = stack2.pop()
+    #     # Return the elements back to stack1
+    #     while stack2:
+    #         element = stack2.pop()
+    #         stack1.append(element)
+    # elif command[0] == '3':
+    #     while stack1:
+    #         element = stack1.pop()
+    #         stack2.append(element)
+    #     print(stack2[-1])
+    #     while stack2:
+    #         element = stack2.pop()
+    #         stack1.append(element)   
+
+    # Approach 3 (Very Expensive enque operation)
     # if command[0] == '1':
     #     # If the stack1 if not empty remove all items
     #     # turn by turn and move them over to stack 2
