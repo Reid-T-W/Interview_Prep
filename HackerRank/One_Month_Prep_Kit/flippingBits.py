ea063047afe2de706b01439da4ed09452019ab66
Reid-T-W <rediettadesse100@gmail.com>
2023-05-16 18:39:18 +0300

    1 month preparation kit

---
+++ HackerRank/One_Month_Prep_Kit/flippingBits.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'flippingBits' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER n as parameter.
+#
+
+def flippingBits(n):
+    # Write your code here
+    flipped = ''
+    bit_str = "{:032b}".format(n)
+    dict = {'0': '1', '1': '0'}
+    for bit in bit_str:
+        flipped += dict[bit]
+    return(int(flipped, 2))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        n = int(input().strip())
+
+        result = flippingBits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
